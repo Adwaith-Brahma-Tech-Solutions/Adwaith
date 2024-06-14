d495fa91b357f46aeeb881db80de70ecbd90a611
naveenkumar123nk <lnaveenkumar123.nk@gmail.com>
2024-06-14 14:24:26 +0530

    added script file

--- Adwaith/Program.cs
+++ Adwaith/Program.cs
@@ -1,4 +1,5 @@
 using Adwaith.Application.Database;
+using Adwaith.Application.Models;
 using Adwaith.Application.Startup;
 using Adwaith.Pages;
 using Microsoft.EntityFrameworkCore;
@@ -12,12 +13,17 @@ using Vite.AspNetCore.Extensions;
 EnvManager.LoadConfig();
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
 builder.Configuration.SetupAppConfig();
 builder.Services.AddAppServices(builder.Configuration);
 
 Log.Logger = new LoggerConfiguration()
     .ReadFrom.Configuration(builder.Configuration).CreateLogger();
 builder.Host.UseSerilog();
+
 
 var app = builder.Build();
 
@@ -45,7 +51,7 @@ app.UseAuthorization();
 app.UseStaticFiles();
 app.UseSession();
 app.UseAntiforgery();
-app.MapRazorComponents<PageRoutes>();
+app.MapRazorComponents<PageRoutes>().AddInteractiveServerRenderMode();
 app.MapMinimalApis<Program>();
 
 app.Services.SetupScheduledJobs();
